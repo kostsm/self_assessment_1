798610bd4453513b7e7e3fb03a8b8274d90c244a
kostsm <78733242+kostsm@users.noreply.github.com>
2024-09-15 16:39:50 +0200

    mistake fixed

--- task4.java
+++ task4.java
@@ -8,10 +8,10 @@ public class task4 {
                 + " or to take arms against a sea of troubles,"
                 + " and by opposing end them?";
         
-		text.toLowerCase();        
+		//text.toLowerCase();        
         
         	// Let's define a word as a sequence of letters, without digits and other symbols
-        	String[] words = text.split("[^a-z]+");
+        	String[] words = text.toLowerCase().split("[^a-z]+");
         
         	bubblesort(words);
 
